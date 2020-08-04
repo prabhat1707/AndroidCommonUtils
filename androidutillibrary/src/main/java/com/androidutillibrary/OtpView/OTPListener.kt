45621e1691210f103db07e20794d04e4a5cc227b
prabhat rai <prabhat.rai@kelltontech.com>
2020-08-05 02:36:00 +0530

    ++ working on Otp View

---
+++ androidutillibrary/src/main/java/com/androidutillibrary/OtpView/OTPListener.kt
@@ -0,0 +1,14 @@
+package com.androidutillibrary.OtpView
+
+interface OTPListener {
+    /**
+     * Callback Fired when user starts typing in the OTP/PIN box.
+     */
+    fun onInteractionListener()
+
+    /**
+     * @param otp Filled OTP
+     * Callback Fired when user has completed filling the OTP/PIN.
+     */
+    fun onOTPComplete(otp: String)
+}
